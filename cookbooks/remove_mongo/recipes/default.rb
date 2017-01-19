93a3b5d1fa3712eee7c93c64e24bedc6ef471ecf
skytap <skytap@host-1.skytap.example>
2017-01-19 12:53:58 -0800

    wrote out recipe for mongo remove

--- cookbooks/remove_mongo/recipes/default.rb
+++ cookbooks/remove_mongo/recipes/default.rb
@@ -1,3 +1,32 @@
 service 'mongod' do
     action [:stop]
 end
+
+rpm_package 'mongodb-org-tools-3.4.1-1.el7.x86_64' do
+     action [:remove]
+end
+
+rpm_package 'mongodb-org-3.4.1-1.el7.x86_64' do
+     action [:remove]
+end
+
+rpm_package 'mongodb-org-shell-3.4.1-1.el7.x86_64' do
+     action [:remove]
+end
+
+rpm_package 'mongodb-org-mongos-3.4.1-1.el7.x86_64' do
+     action [:remove]
+end
+
+rpm_package 'mongodb-org-server-3.4.1-1.el7.x86_64' do
+     action [:remove]
+end
+
+file '/var/log/mongodb' do
+     action [:delete]
+end
+
+file /var/lib/mongo' do
+     action [:delete]
+end
+
